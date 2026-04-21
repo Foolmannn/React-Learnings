92de3b0644a17dc81b874b01db1ed35553d132b3
Suman Pun Magar <mannmagar19@gmail.com>
2026-04-21 12:46:26 +0545

    Random password generator

--- 06Project2/src/App.jsx
+++ 06Project2/src/App.jsx
@@ -1,43 +1,97 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from "react";
 
-import './index.css'
-
-
-
-
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [length, setlength] = useState(8)
-  const [wantNumber, setwantNumber] = useState(false)
-  const [wantSymbol, setwantSymbol] = useState(false)
-  const [password, setpassword]= useState("")
+  const [length, setlength] = useState(8);
+  const [wantNumber, setwantNumber] = useState(false);
+  const [wantSymbol, setwantSymbol] = useState(false);
+  const [password, setpassword] = useState("");
 
-  const passwordGenerator = ()=>{
-    
-  }
+  const passwordGenerator = useCallback(() => {
+      let pass = "";
+      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      if (wantNumber) str += "0123456789";
+      if (wantSymbol) str += "!@#$%^&*()_{}[]~'-+=";
+
+      for (let i = 1; i <= length; i++) {
+        let char = Math.floor(Math.random() * str.length + 1);
+        pass += str.charAt(char);
+      }
+      setpassword(pass);
+    }, [wantNumber, wantSymbol, length, setpassword]);
+  // passwordGenerator() this will not react as it will create loop  so we need to create another button that calls this callback hook
+  console.log(length);
+  console.log(password);
+  console.log(wantSymbol);
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, wantNumber, wantSymbol, passwordGenerator]);
 
   return (
-  <>
-
-   <h1 className="text-3xl text-white flex flex-wrap justify-center font-bold ">Random Password Generator</h1>
-    <div className="main bg-amber-50 min-h-2/4  ">
-
-
-      <div className="texbox flex flex-wrap rounded-b-4xl bg-white justify-center p-0 m-0">
-        <div className="placeholder bg-white w-2/3 "></div>
-        <div className="copy bg-blue-700 px-5 py-2 ">Copy</div>
-
+    <>
+      <h1 className="text-3xl text-white flex flex-wrap justify-center font-bold ">
+        Random Password Generator
+      </h1>
+      <div className="main w-full max-w-1/2 mx-auto my-10 bg-gray-700 p-10 rounded-xl font-medium text-xl ">
+        <div className="flex rounded-xl bg-white justify-between p-0 m-0">
+          <input
+            type="text"
+            value={password}
+            placeholder="password...."
+              readOnly
+            className="py-3 px-3 w-full "
+          />
+          <button className="outline-none bg-blue-700 text-white py-1 px-3 cursor-pointer rounded-e-xl ">
+            Copy
+          </button>
+          {/* <button onClick={passwordGenerator} className="outline-none bg-blue-700 text-white py-1 px-3 cursor-pointer rounded-e-xl ">
+            Generate
+          </button> */}
+        </div>
+        <div className="options px-5 py-2 flex items-center gap-x-5 text-xl font-medium bg-lime-200 rounded-md  my-3">
+          <div className="">
+            <label htmlFor="">Length:</label>
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setlength(Number(e.target.value));
+              }}
+            />
+          </div>
+          <div className="">
+            <input
+              type="checkbox"
+              defaultChecked={wantNumber}
+              id="numberInput"
+              className="cursor-pointer"
+              onChange={() => {
+                setwantNumber((prev) => !prev)
+              }}
+            />
+              <label htmlFor="numberInput">Numbers </label>
+          </div>
+          <div className="">
+            <input
+              type="checkbox"
+              defaultChecked={wantSymbol}
+              id="symbolInput"
+              className="cursor-pointer"
+              onChange={() => {
+                setwantSymbol((prev) => !prev)
+              }}
+            />
+              <label htmlFor="symbolInput">Symbols </label>
+          </div>
+        </div>
       </div>
-    </div>
-
-
-
-
-
-
-  </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
