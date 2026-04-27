83dacd43d1f84846e7f6de87b38d07eddbe33d34
Suman Pun Magar <mannmagar19@gmail.com>
2026-04-27 11:41:53 +0545

    Project 3  TODO - Local Storage fixes

--- ContextApiProject-3/project3-Todo/src/App.jsx
+++ ContextApiProject-3/project3-Todo/src/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useEffectEvent, useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts/index";
+import { TodoForm, TodoItem } from "./components";
 
 function App() {
   const [todos, settodos] = useState([]);
@@ -10,8 +11,15 @@ function App() {
 
   const updateTodo = (id, todo) => {
     settodos((prev) =>
-      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
-    ); // this matches the id of the previous todo and update it if the id matches else keep the previous todo as it is
+    //   prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
+    // );
+     // this matches the id of the previous todo and update it if the id matches else keep the previous todo as it is
+
+     // Better approach is : 
+    prev.map((prevTodo) =>
+  prevTodo.id === id ? { ...prevTodo, ...todo } : prevTodo
+))
+// why better above was replacing the whole object → might lose properties like id
 
     // Above is same as below
     // prev.map((eachval) =>{
@@ -30,12 +38,31 @@ function App() {
   const toggleComplete = (id) => {
     settodos((prev) =>
       prev.map((prevTodo) =>
-        prevTodo === id
+        prevTodo.id === id
           ? { ...prevTodo, completed: !prevTodo.completed }
           : prevTodo,
       ),
     );
   };
+
+  // Local storage setting and getting 
+
+  useEffect(() => {
+      const storedTodos = JSON.parse(localStorage.getItem("todos"));
+      
+      if (storedTodos && storedTodos.length > 0) {
+          settodos(storedTodos);
+        }
+    }, []);
+    //   const [todos, settodos] = useState(() => {
+    //   const storedTodos = localStorage.getItem("todos");
+    //   return storedTodos ? JSON.parse(storedTodos) : [];
+    // });
+    // This is more efficent way as no two use effect is needed 
+    
+useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+},[todos])
 
   return (
     <>
@@ -47,9 +74,16 @@ function App() {
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">
               Manage Your Todos
             </h1>
-            <div className="mb-4">{/* Todo form goes here */}</div>
+            <div className="mb-4">{/* Todo form goes here */}<TodoForm/></div>
             <div className="flex flex-wrap gap-y-3">
               {/*Loop and Add TodoItem here */}
+              {todos.map((todo) =>(
+                <div key={todo.id}
+                    className='w-full'
+                >
+                    <TodoItem todo={todo}/>
+                </div>
+              ))}
             </div>
           </div>
         </div>
