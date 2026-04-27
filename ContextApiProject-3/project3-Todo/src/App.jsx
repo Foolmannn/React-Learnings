acd94e5e6f20373d6be63a70d1017e24cb6675f2
Suman Pun Magar <mannmagar19@gmail.com>
2026-04-27 10:14:23 +0545

    Project 3 for context api TODO

---
+++ ContextApiProject-3/project3-Todo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useEffectEvent, useState } from "react";
+
+
+
+
+function App() {
+
+
+
+  return (
+<>
+<h1 className="font-bold text-3xl bg-emerald-300 text-center  ">To do </h1>
+</>
+  );
+}
+
+export default App;
