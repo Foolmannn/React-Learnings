b538fb5ab25325cee887be6d31b04dfa8fbac0a0
Suman Pun Magar <mannmagar19@gmail.com>
2026-04-28 11:52:15 +0545

    Learning redux-toolkit

--- ReduxToolkit/redux-toolkit-todo/src/App.jsx
+++ ReduxToolkit/redux-toolkit-todo/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 <>
-<h1>
+<h1 className='text-center text-3xl font-bold '>
   Learning about redux-toolkit
   </h1>
+  <AddTodo/>
+  <Todos/>
   </>
   )
 }
