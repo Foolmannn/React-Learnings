231dd55a0396882adb8787a17c2553c8064230ef
Suman Pun Magar <mannmagar19@gmail.com>
2026-04-30 11:18:23 +0545

    Setup for the Mega Blog site ENV and production grade

---
+++ BlogSite/src/conf/config.js
@@ -0,0 +1,12 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
+
+
+// This is the productin grade configuartion so that these varaible will be sent as string is guarenteed and the we donot have to write the whole. import. meta. env every time
