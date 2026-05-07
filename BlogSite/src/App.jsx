61a819f3e5e878454369235a046ac3572c7aab35
Suman Pun Magar <mannmagar19@gmail.com>
2026-05-07 19:10:18 +0545

    Setting redux toolkit

--- BlogSite/src/App.jsx
+++ BlogSite/src/App.jsx
@@ -1,17 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice.js";
+import { Footer, Header } from "./components/index.js";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  // This if for create-vite-app 
-// console.log(process.env.REACT_APP_APPWRITE_URL)
-// For the vite we have to write as :
-console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-<>
-<h1>Test </h1>
-</>
-  )
+  // This if for create-vite-app
+  // console.log(process.env.REACT_APP_APPWRITE_URL)
+  // For the vite we have to write as :
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
