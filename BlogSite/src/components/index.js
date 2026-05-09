f2d10f255ad66eb230c138f05503fe72ad859e83
Suman Pun Magar <mannmagar19@gmail.com>
2026-05-09 21:24:19 +0545

    Blog project postcard and select box components

--- BlogSite/src/components/index.js
+++ BlogSite/src/components/index.js
@@ -3,7 +3,9 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
-
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
 export {
-    Header,Footer,Container,Logo,LogoutBtn
+    Header,Footer,Container,Logo,LogoutBtn,Input,PostCard,Select
 }
