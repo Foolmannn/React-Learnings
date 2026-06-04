ae46ce469666bd8b6518cf29877b0d6c424d6895
Foolmannn <mannmagar19@gmail.com>
2026-06-04 11:30:35 +0000

    react - es6 - template Strings

---
+++ from_w3/3React_ES6/templateStrings.js
@@ -0,0 +1,81 @@
+// Template Strings
+// Template strings allow you to write strings that span multiple lines and include embedded expressions:
+
+// Before:
+const name = "John";
+const age = 30;
+const message = "Hello, " + name + "!\n" + 
+"You are " + age + " years old.";
+
+console.log(message)
+// Example
+// With Template Strings:
+
+
+// Template strings use backticks (`) instead of quotes and can include:
+
+// Multiple lines without \n
+// Expressions inside ${}
+// Quotes without escaping
+
+
+// Multi-line Strings:
+
+
+// Note: The indentation in multi-line strings becomes part of the string.
+
+
+// Example
+// The indentation becomes part of the string:
+
+
+
+// Expression Interpolation
+// You can include any valid JavaScript expression inside ${} in a template string:
+
+// Example
+// Insert variables inside template strings:
+
+// Example
+// Insert expressions inside template strings:
+
+// Example
+// Using the map function inside template strings:
+
+
+// Example
+// Using ternery operator inside template strings:
+
+
+// Tagged Templates
+// You can also use template strings with a function (called a tag) to modify the output.
+
+// Note: Tagged templates are an advanced feature. You might not need them in most cases.
+
+// The function takes the text and the expression(s) as arguments.
+
+// Look at the example below:
+
+
+// Example
+// Tagged Template:
+
+
+// Example Explained
+// The function name is highlight, you can name it whatever you want.
+
+// The first argument holds the text between the expressions, as an array.
+// The array items are splitted by the expression.
+// In this example strings[0] holds "Hello " and strings[1] holds " how are you?".
+
+// The second argument holds the expressions. In this example fname holds "John".
+
+// Inside the function you can use the arguments to create the final string, and return it.
+
+// Note: If the template string contains multiple expressions, the text will still be held in the first argument, but the expressions will either be held in multiple arguments, or as an array in the second argument.
+
+// Example
+// Tagged Template with multiple expressions:
+
+// Example
+// Tagged Template with multiple expressions that are held in an array using the rest parameter:
