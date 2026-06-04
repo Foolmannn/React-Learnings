3c8094440cd1d62ac2dde529ffa7990404691064
Foolmannn <mannmagar19@gmail.com>
2026-06-04 11:12:41 +0000

    react - es6 modules - export - default

---
+++ from_w3/3React_ES6/modules/message.js
@@ -0,0 +1,16 @@
+// DEFAULT EXPORTS
+
+// Let us create another file, named message.js, and use it for demonstrating default export.
+
+// You can only have one default export in a file.
+
+const message = ()=>{
+
+    const name = "Suman";
+    const age = 20;
+    return name + 'is' + age 
++  "years old"
+};
+
+export default message;
+
