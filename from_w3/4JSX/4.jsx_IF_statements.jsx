4b077c9276b148df82a5d8c8ae75babfee48c24c
Foolmannn <mannmagar19@gmail.com>
2026-06-05 10:41:55 +0000

    recat - jsx  - if statements

--- from_w3/4JSX/4.jsx_IF_statements.jsx
+++ from_w3/4JSX/4.jsx_IF_statements.jsx
@@ -9,12 +9,28 @@
 
 
 // Write "Banana" if x is less than 10, otherwise "Apple":
+function Fruit(){
+    const x = 10;
+    let y = "Apple"
+    if(x>10){
+        y = "Banana"
+    }
 
-
+    return (
+        <h1>{y}</h1>
+    )
+}
 
 // Option 2:
 // Use ternary expressions instead:
 
 // Write "Banana" if x is less than 10, otherwise "Apple":
+
+function Fruit(){
+    const x = 9;
+    return (
+        <h1>{(x)>10? "Banana": "Apple"}</h1>
+    )
+}
 
 // Note that in order to embed a JavaScript expression inside JSX, the JavaScript must be wrapped with curly braces, {}.
