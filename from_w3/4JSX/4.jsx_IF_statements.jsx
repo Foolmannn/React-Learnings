fe06f7411d3e1a30ce55e9f22405a9a50b635d59
Foolmannn <mannmagar19@gmail.com>
2026-06-05 10:36:49 +0000

    recat - jsx  - if statements

---
+++ from_w3/4JSX/4.jsx_IF_statements.jsx
@@ -0,0 +1,20 @@
+// React JSX If Statements
+// Conditions - if statements
+// React supports if statements, but not inside JSX.
+
+// To be able to use conditional statements in JSX, you should put the if statements outside of the JSX, or you could use a ternary expression instead:
+
+// Option 1:
+// Write if statements outside of the JSX code:
+
+
+// Write "Banana" if x is less than 10, otherwise "Apple":
+
+
+
+// Option 2:
+// Use ternary expressions instead:
+
+// Write "Banana" if x is less than 10, otherwise "Apple":
+
+// Note that in order to embed a JavaScript expression inside JSX, the JavaScript must be wrapped with curly braces, {}.
