07d9acb6df2e1b5e084c0b0679c94a01f28c5aec
Foolmannn <mannmagar19@gmail.com>
2026-06-06 13:11:05 +0000

    react - components

---
+++ from_w3/Components.jsx
@@ -0,0 +1,15 @@
+React Components
+Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.
+
+Components come in two types, Class components and Function components, in this tutorial we will concentrate on Function components.
+
+In older React code bases, you may find Class components primarily used.
+
+It is now suggested to use Function components along with Hooks, instead of Class components.
+
+Class components are still supported, check the Class components section for more information.
+
+Create Your First Component
+When creating a React component, the component's name MUST start with an upper case letter.
+
+React components returns HTML code.
