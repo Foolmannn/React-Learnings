3403e21cbf94ff3dbd9ded4a572ae4f5fabdd227
Foolmannn <mannmagar19@gmail.com>
2026-06-07 15:09:51 +0000

    react - class components

--- from_w3/5ClassComponents.jsx
+++ from_w3/5ClassComponents.jsx
@@ -437,30 +437,30 @@ componentDidUpdate()
 // Example:
 // If the component gets updated, the getDerivedStateFromProps() method is called:
 
-// class Header extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {favoritecolor: "red"};
-//   }
-//   static getDerivedStateFromProps(props, state) {
-//     return {favoritecolor: props.favcol };
-//   }
-//   changeColor = () => {
-//     this.setState({favoritecolor: "blue"});
-//   }
-//   render() {
-//     return (
-//       <div>
-//       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-//       <button type="button" onClick={this.changeColor}>Change color</button>
-//       </div>
-//     );
-//   }
-// }
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    return {favoritecolor: props.favcol };
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
 
-// createRoot(document.getElementById('root')).render(
-//   <Header favcol="yellow" />
-// );
+createRoot(document.getElementById('root')).render(
+  <Header favcol="yellow" />
+);
 
 // shouldComponentUpdate
 // In the shouldComponentUpdate() method you can return a Boolean value that specifies whether React should continue with the rendering or not.
@@ -472,58 +472,58 @@ componentDidUpdate()
 // Example:
 // Stop the component from rendering at any update:
 
-// class Header extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {favoritecolor: "red"};
-//   }
-//   shouldComponentUpdate() {
-//     return false;
-//   }
-//   changeColor = () => {
-//     this.setState({favoritecolor: "blue"});
-//   }
-//   render() {
-//     return (
-//       <div>
-//       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-//       <button type="button" onClick={this.changeColor}>Change color</button>
-//       </div>
-//     );
-//   }
-// }
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  shouldComponentUpdate() {
+    return false;
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
 
-// createRoot(document.getElementById('root')).render(
-//   <Header />
-// );
+createRoot(document.getElementById('root')).render(
+  <Header />
+);
 
 // Example:
 // Same example as above, but this time the shouldComponentUpdate() method returns true instead:
 
-// class Header extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {favoritecolor: "red"};
-//   }
-//   shouldComponentUpdate() {
-//     return true;
-//   }
-//   changeColor = () => {
-//     this.setState({favoritecolor: "blue"});
-//   }
-//   render() {
-//     return (
-//       <div>
-//       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-//       <button type="button" onClick={this.changeColor}>Change color</button>
-//       </div>
-//     );
-//   }
-// }
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  shouldComponentUpdate() {
+    return true;
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
 
-// createRoot(document.getElementById('root')).render(
-//   <Header />
-// );
+createRoot(document.getElementById('root')).render(
+  <Header />
+);
 
 // render
 // The render() method is of course called when a component gets updated, it has to re-render the HTML to the DOM, with the new changes.
@@ -533,29 +533,30 @@ componentDidUpdate()
 // Example:
 // Click the button to make a change in the component's state:
 
-// class Header extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {favoritecolor: "red"};
-//   }
-//   changeColor = () => {
-//     this.setState({favoritecolor: "blue"});
-//   }
-//   render() {
-//     return (
-//       <div>
-//       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-//       <button type="button" onClick={this.changeColor}>Change color</button>
-//       </div>
-//     );
-//   }
-// }
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
 
-// createRoot(document.getElementById('root')).render(
-//   <Header />
-// );
+createRoot(document.getElementById('root')).render(
+  <Header />
+);
 
 // getSnapshotBeforeUpdate
+
 // In the getSnapshotBeforeUpdate() method you have access to the props and state before the update, meaning that even after the update, you can check what the values were before the update.
 
 // If the getSnapshotBeforeUpdate() method is present, you should also include the componentDidUpdate() method, otherwise you will get an error.
