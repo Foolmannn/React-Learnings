8882d9890b7138c0ccd7bdd5b1d7757e8aa5241d
Foolmannn <mannmagar19@gmail.com>
2026-06-14 11:36:56 +0000

    React - Hooks - useRef hook

--- from_w3/Hooks/4useRefHook.jsx
+++ from_w3/Hooks/4useRefHook.jsx
@@ -47,11 +47,11 @@ createRoot(document.getElementById('root')).render(
 
 // When we initialize useRef we set the initial value: useRef(0).
 
-// It's like doing this: const count = {current: 0}. We can access the count by using count.current.
+// IT'S LIKE DOING THIS: CONST COUNT = {CURRENT: 0}. WE CAN ACCESS THE COUNT BY USING COUNT.CURRENT.
 
-// REMOVE ADS
 
-// Accessing DOM Elements
+// ACCESSING DOM ELEMENTS
+
 // The useRef Hook is often used to access DOM elements directly.
 
 // First, we create a ref using the useRef Hook: const inputElement = useRef();.
@@ -63,15 +63,67 @@ createRoot(document.getElementById('root')).render(
 // Example:
 // Use useRef to focus the input:
 
+import { useRef } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  const inputElement = useRef();
+
+  const focusInput = () => {
+    inputElement.current.focus();
+  };
+
+  return (
+    <>
+      <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <App />
+);
+
 // In the example, the input field gets focus when the button is clicked, because the onClick function calls inputElement.current.focus().
 
-// Tracking State Changes
+
+// TRACKING STATE CHANGES
+
 // The useRef Hook can also be used to keep track of previous state values.
 
 // This is because we are able to persist useRef values between renders.
 
 // Example:
 // Use useRef to keep track of previous state values:
+
+import { useRef, useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const previousInputValue = useRef("");
+
+  useEffect(() => {
+    previousInputValue.current = inputValue;
+  }, [inputValue]);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h2>Current Value: {inputValue}</h2>
+      <h2>Previous Value: {previousInputValue.current}</h2>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <App />
+);
 
 // This time we use a combination of useState, useEffect, and useRef to keep track of the previous state.
 
