5168d29883f5d5ab8c00ec80a4c288fdb7c930a8
Foolmannn <mannmagar19@gmail.com>
2026-06-15 12:03:43 +0000

    react - CSS - styling

--- from_w3/18.CSS_Styling.jsx
+++ from_w3/18.CSS_Styling.jsx
@@ -1,48 +1,100 @@
 // Styling React Using CSS
+
 // There are many ways to style React with CSS, this tutorial will take a closer look at three common ways:
 
 // Inline styling
 // CSS stylesheets
 // CSS Modules
-// Inline Styling
+
+
+// INLINE STYLING
 // To style an element with the inline style attribute, the value must be a JavaScript object:
 
 // Example:
 // Insert an object with the styling information:
 
+const Header=()=>{
+    return  (
+        <>
+        <h1 style={{color:"blue"}}>Hello World ! </h1>
+        <p style={{backgroundColor:"aqua", fontFamily:"sans-serif"}}>This world is beautiful , isn't it ?</p>
+        </>
+    )
+}
 
-// Note: In JSX, JavaScript expressions are written inside curly braces, and since JavaScript objects also use curly braces, the styling in the example above is written inside two sets of curly braces {{}}.
+
+// NOTE: IN JSX, JAVASCRIPT EXPRESSIONS ARE WRITTEN INSIDE CURLY BRACES, AND SINCE JAVASCRIPT OBJECTS ALSO USE CURLY BRACES, THE STYLING IN THE EXAMPLE ABOVE IS WRITTEN INSIDE TWO SETS OF CURLY BRACES {{}}.
 
 // camelCased Property Names
 // Since the inline CSS is written in a JavaScript object, properties with hyphen separators, like background-color, must be written with camel case syntax:
 
-// Example:
-// Use backgroundColor instead of background-color:
 
-// JavaScript Object
+
+// JAVASCRIPT OBJECT
 // You can also create an object with styling information, and refer to it in the style attribute:
 
 // Example:
 // Create a style object named myStyle:
 
-// CSS Stylesheet
+const Header = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
+  return (
+    <>
+      <h1 style={myStyle}>Styling using the object </h1>
+    </>
+  );
+}
+
+
+// CSS STYLESHEET
 // You can write your CSS styling in a separate file, just save the file with the .css file extension, and import it in your application.
 
 // Example
 // Create a new file called "MyStylesheet.css" and insert some CSS code in it:
 
+// mystylesheet.css 
+// body {
+//   background-color: #282c34;
+//   color: white;
+//   padding: 40px;
+//   font-family: Sans-Serif;
+//   text-align: center;
+// }
 // Note: You can call the file whatever you like, just remember the correct file extension.
 
 // Import the stylesheet in your application:
 
 // Example
 // Use the import statement to import the stylesheet:
+// index.js
+import { createRoot } from 'react-dom/client';
+import './MyStylesheet.css';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <Header />
+);
 
 // React knows that files imported with the .css extension, should be treated as a CSS Stylesheet.
 
 // Note: The styles will only be available for the component that imported it.
 
-// CSS Modules
+
+
+// CSS MODULES
 // Another way of adding styles to your application is to use CSS Modules.
 
 // CSS Modules are convenient for components that are placed in separate files.
@@ -53,11 +105,26 @@
 
 // Example
 // Create a new file called "my-style.module.css" and insert some CSS code in it:
-
+// my-style.module.css
+.bigred {
+  color: Tomato;
+  padding: 40px;
+  font-family: Sans-Serif;
+  text-align: center;
+}
 
 // Import the stylesheet in your component:
 
 // Example
 // Use import styles to import the stylesheet:
-
-// You will learn more about CSS Modules in the next chapter.
+index.jsx
+import { createRoot } from 'react-dom/client';
+import styles from './my-style.module.css'; 
+  
+const Car = () => {
+  return <h1 className={styles.bigred}>Hello Car!</h1>;
+}
+  
+createRoot(document.getElementById('root')).render(
+  <Car />
+);
---
+++ from_w3/testFolder/src/Test.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Test() {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
+  return (
+    <>
+      <h1 style={myStyle}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
+
+export default Test
