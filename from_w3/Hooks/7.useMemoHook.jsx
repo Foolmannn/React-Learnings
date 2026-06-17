415d3a5bfda6614bbe05741b300f45e4dc026110
Foolmannn <mannmagar19@gmail.com>
2026-06-17 11:22:31 +0000

    React - useMemo hook

--- from_w3/Hooks/7.useMemoHook.jsx
+++ from_w3/Hooks/7.useMemoHook.jsx
@@ -1,4 +1,5 @@
 // React useMemo Hook
+
 // The React useMemo Hook returns a memoized value.
 
 // Think of memoization as caching a value so that it does not need to be recalculated.
@@ -13,17 +14,66 @@
 
 // useCallback returns a memoized function.
 
-// Learn more about useCallback in the useCallback chapter.
 
 // Without useMemo
+
 // The useMemo Hook can be used to keep expensive, resource intensive functions from needlessly running.
 
 // In this example, we have an expensive function that runs on every render.
 
 // When changing the count or adding a todo, you will notice a delay in execution.
 
-// Example:Get your own React.js Server
+// Example:
 // A poor performing function. The expensiveCalculation function runs on every render:
+
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+        <p>Note that this example executes the expensive function also when you click on the Add Todo button.</p>
+      </div>
+    </div>
+  );
+};
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
+
+createRoot(document.getElementById('root')).render(
+  <App />
+);
+
 
 // Use useMemo
 // To fix this performance issue, we can use the useMemo Hook to memoize the expensiveCalculation function. This will cause the function to only run when needed.
@@ -36,3 +86,50 @@
 
 // Example:
 // Performance example using the useMemo Hook:
+
+import { useState, useMemo } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </div>
+  );
+};
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
+
+createRoot(document.getElementById('root')).render(
+  <App />
+);
